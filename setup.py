4a66bf05af3446a2b34f5fd13d476784463e0aab
joseph <joseph@yknet.cn>
2020-05-03 07:55:08 +0800

    no msg

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     test_suite='test_socketio',
     entry_points={
         'console_scripts': [
-            'i_socketio=i_socketio.server:main'
+            'i_socketio=i_socketio.app:main'
         ],
     },
     classifiers=[
